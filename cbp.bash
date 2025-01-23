719083257f5329f5d1ac1e4beaf2203d12bf6f2a
NulledBird <82178308+NulledBird@users.noreply.github.com>
2025-01-23 18:12:00 -0500

    i forgor something mb

--- cbp.bash
+++ cbp.bash
@@ -74,7 +74,7 @@ echo -e "Contents of /etc/cron.yearly: $(ls -CF /etc/cron.yearly)"
 
 echo -e "\n\nChecking for suspicious files..."
 
-find /home/ -mindepth 1 -maxdepth 1 -type d \( ! -name . \) -exec bash -c "pwd && echo -e '	/Videos/: $(ls -CF ./Videos/)\n	/Music/: $(ls -CF ./Music/)\n	/Pictures/: $(ls -CF ./Pictures/)\n	/.bash_alias: $(cat ./.bash_alias 2>/dev/null)'" \;
+find /home/ -mindepth 1 -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && echo -e '	/Videos/: $(ls -CF ./Videos/)\n	/Music/: $(ls -CF ./Music/)\n	/Pictures/: $(ls -CF ./Pictures/)\n	/.bash_alias: $(cat ./.bash_alias 2>/dev/null)'" \;
 
 
 echo -e "\n\n\n\n"
